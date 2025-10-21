80ceb0aa2f2fec34b2812befd5f1826eb6f9eff8
Linx306 <hassam306@gmail.com>
2025-10-21 10:28:45 -0600

    Se agrego galeria, y uso de tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { GalleryComponent } from './gallery.component';
+import { HomeComponent } from './home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'galeria', component: GalleryComponent }
+];
