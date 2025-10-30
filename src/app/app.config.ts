9853c5c275971775b9ec62141665366907755fba
MarcAnthonyLG <156326295+BukiLopez@users.noreply.github.com>
2025-10-30 11:06:06 -0600

    Add Firebase integration and update Angular config
    Introduced Firebase support by adding firebase.json, updating .gitignore, and creating firebase.service.ts. Updated Angular project naming in angular.json, added @angular/fire and firebase dependencies, and made related changes in app.config.ts and gallery components.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "landing-page-a7576", appId: "1:871772196371:web:ef77ae740db3dc8d4b39e6", storageBucket: "landing-page-a7576.firebasestorage.app", apiKey: "AIzaSyCT8-61ziVv8hKLG7XP-H9ZWj1EqOqhFUo", authDomain: "landing-page-a7576.firebaseapp.com", messagingSenderId: "871772196371", measurementId: "G-L0TJ03CEEG" })), provideFirestore(() => getFirestore())
   ]
 };
